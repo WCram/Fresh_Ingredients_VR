11201a646c2eb2a2a51c69cf442b3017e05070d7
WCram <wcram@live.com>
2020-02-10 20:22:57 -0800

    Added RCHit script.

---
+++ Assets/Project_Assets/Scripts/RCHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCHit : MonoBehaviour
+{
+
+    public float rayLength;
+    RaycastHit rch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayLength = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * rayLength, Color.red, 0.5f);
+        if (Physics.Raycast(transform.position, transform.forward, out rch, 2.0F))
+        {
+            if(rch.transform.tag == "button")
+            {
+                Debug.Log("Button Touched");
+            }
+        } 
+    }
+}
